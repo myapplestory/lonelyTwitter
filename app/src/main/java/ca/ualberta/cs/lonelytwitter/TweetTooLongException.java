d215f671f69f598293586f11146eadeb47c9e9d0
csun <csun@ualberta.ca>
2018-01-18 18:11:42 -0700

    lab demo'

---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
@@ -0,0 +1,14 @@
+package ca.ualberta.cs.lonelytwitter;
+
+
+public class TweetTooLongException extends Exception {
+
+
+
+
+
+
+
+
+
+}
---
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweetable.java
@@ -0,0 +1,17 @@
+package ca.ualberta.cs.lonelytwitter;
+
+
+import java.util.Date;
+
+public interface Tweetable {
+    public String GetMessage();
+    public Date getDate();
+
+
+
+
+
+
+
+
+}
